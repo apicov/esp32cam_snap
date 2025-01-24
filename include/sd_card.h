0e3b3948f35cdeae9303ee1173946c87af3742d0
Francisco Jurado <fjurado@cimat.mx>
2025-01-24 11:26:31 +0100

    initialize _all_ the files of "mount_config"

--- include/sd_card.h
+++ include/sd_card.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <esp_err.h>
 #include "driver/sdmmc_host.h"
-#include "driver/sdmmc_defs.h"
 #include "sdmmc_cmd.h"
 #include "esp_vfs_fat.h"
 
--- src/sd_card.cpp
+++ src/sd_card.cpp
@@ -5,13 +5,8 @@ esp_err_t initi_sd_card(void)
 {  
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-        .format_if_mount_failed = false,
-        .max_files = 3,
-        .allocation_unit_size = 16 * 1024, // Default allocation unit size, adjust as needed
-        .disk_status_check_enable = false, // Disable disk status check if not required
-        //.use_one_fat = true,  // Use only one FAT partition
-    };
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = VFS_FAT_MOUNT_DEFAULT_CONFIG();
+    mount_config.allocation_unit_size = 16 * 1024;
     
     sdmmc_card_t *card;
     esp_err_t err = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
