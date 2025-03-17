a124e04fe3322f4094a9ba28e397fcb4b63a4447
Francisco Jurado <fjurado@cimat.mx>
2025-03-17 12:19:31 +0100

    use specific member names in structure initialization.

--- components/camera_ctl/camera_ctl.cpp
+++ components/camera_ctl/camera_ctl.cpp
@@ -35,35 +35,37 @@
 esp_err_t CameraCtl::init_camera(void)
 {
     camera_config_t config = {
-    CAM_PIN_PWDN,        // pin_pwdn
-    CAM_PIN_RESET,       // pin_reset
-    CAM_PIN_XCLK,        // pin_xclk
-    CAM_PIN_SIOD,        // pin_sccb_sda
-    CAM_PIN_SIOC,        // pin_sccb_scl
-    CAM_PIN_D7,          // pin_d7
-    CAM_PIN_D6,          // pin_d6
-    CAM_PIN_D5,          // pin_d5
-    CAM_PIN_D4,          // pin_d4
-    CAM_PIN_D3,          // pin_d3
-    CAM_PIN_D2,          // pin_d2
-    CAM_PIN_D1,          // pin_d1
-    CAM_PIN_D0,          // pin_d0
-    CAM_PIN_VSYNC,       // pin_vsync
-    CAM_PIN_HREF,        // pin_href
-    CAM_PIN_PCLK,        // pin_pclk
-    CONFIG_XCLK_FREQ,    // xclk_freq_hz
-    LEDC_TIMER_0,        // ledc_timer
-    LEDC_CHANNEL_0,      // ledc_channel
-    PIXFORMAT_JPEG,//PIXFORMAT_GRAYSCALE, //PIXFORMAT_JPEG,      // pixel_format
-    FRAMESIZE_QQVGA,      // frame_size
-    12,                   // jpeg_quality
-    1,                    // fb_count
-    CAMERA_FB_IN_PSRAM,   // fb_location (or CAMERA_FB_IN_DRAM)
-    CAMERA_GRAB_WHEN_EMPTY, // grab_mode
-    I2C_NUM_0 // sccb_i2c_port
+      .pin_pwdn = CAM_PIN_PWDN,
+      .pin_reset = CAM_PIN_RESET,
+      .pin_xclk = CAM_PIN_XCLK,
+      .pin_sccb_sda = CAM_PIN_SIOD,
+      .pin_sccb_scl = CAM_PIN_SIOC,
+      .pin_d7 = CAM_PIN_D7,
+      .pin_d6 = CAM_PIN_D6,
+      .pin_d5 = CAM_PIN_D5,
+      .pin_d4 = CAM_PIN_D4,
+      .pin_d3 = CAM_PIN_D3,
+      .pin_d2 = CAM_PIN_D2,
+      .pin_d1 = CAM_PIN_D1,
+      .pin_d0 = CAM_PIN_D0,
+      .pin_vsync = CAM_PIN_VSYNC,
+      .pin_href  = CAM_PIN_HREF,
+      .pin_pclk  = CAM_PIN_PCLK,
+
+      .xclk_freq_hz = CONFIG_XCLK_FREQ,
+      .ledc_timer = LEDC_TIMER_0,
+      .ledc_channel = LEDC_CHANNEL_0,
+
+      .pixel_format = PIXFORMAT_JPEG,
+      .frame_size = FRAMESIZE_QQVGA,
+
+      .jpeg_quality = 12,
+      .fb_count = 1,
+      .fb_location = CAMERA_FB_IN_PSRAM,
+      .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+      .sccb_i2c_port = I2C_NUM_0
     };
 
-    ESP_RETURN_ON_ERROR(camera_xclk_init(20'000'000), TAG, "xclk_init");
     ESP_RETURN_ON_ERROR(esp_camera_init(&config), TAG, "camera_init");
     return ESP_OK;
 }
