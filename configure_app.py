8985e20746d34f8791e110e75e40f60eb5cb3b1d
Francisco Jurado <fjurado@cimat.mx>
2025-05-03 10:37:03 +0200

    set mosquitto's default port if no port is given

--- configure_app.py
+++ configure_app.py
@@ -28,6 +28,8 @@ def header_string():
     host, port = uri if len(uri) > 1 \
         else [uri[0], input('Enter the MQTT port: ')]
 
+    port = port or 1883 # mosquitto's default port
+
     cmd_topic = input(f"MQTT topic to read commands from [{default_cmd}]: ") \
         or default_cmd
 
@@ -68,26 +70,30 @@ else:
     import builtins
     builtins.input = lambda _ : answers.pop(0)
 
-    expect = '''
+    def expect(port=1234):
+        return f'''
 #ifndef APP_CONFIGURATION_H
 #define APP_CONFIGURATION_H
 
 #define SSID "my_ssid"
 #define PASSWORD "my_passwd"
-#define MQTT_URI "mqtt://127.0.0.1:1234"
+#define MQTT_URI "mqtt://127.0.0.1:{port}"
 #define MQTT_CMD_TOPIC "/camera/cmd"
 #define MQTT_IMG_TOPIC "/camera/img"
 
 #endif
 '''
+
     answers = ['my_ssid', 'my_passwd', 'mqtt://127.0.0.1:1234', '', '']
-    assert header_string() == expect
+    assert header_string() == expect()
 
     answers = ['my_ssid', 'my_passwd', '127.0.0.1', '1234', '', '']
-    assert header_string() == expect
+    assert header_string() == expect()
+
+    answers = ['my_ssid', 'my_passwd', '127.0.0.1', '', '', '']
+    assert header_string() == expect(1883)
 
     answers = ['my_ssid', 'my_passwd', '127.0.0.1:1234', '', '']
-    assert header_string() == expect
-
+    assert header_string() == expect()
 
     print("ok")
